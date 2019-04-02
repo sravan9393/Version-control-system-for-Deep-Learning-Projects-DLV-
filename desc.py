efa241d86814875581a94429f45a08f2fe4cbbf4
Pradeepika Kolluru <31911136+pradeepika1997@users.noreply.github.com>
2019-04-02 08:13:39 -0700

    Listing all Models, providing description to model, Uploading files to DLV server

---
+++ desc.py
@@ -0,0 +1,109 @@
+import os
+import sys
+import json
+import global_config
+import pandas as pd
+
+def handle_options_desc(cmd_parser):
+
+    cmd_parser.add_argument('-v', '--version',
+                          help="version",
+                          action="store_true")
+
+    cmd_parser.add_argument('-u','--username',
+                          nargs=1,
+                          dest='username',
+                          help='Enter the author address')
+
+    cmd_parser.add_argument('-m','--model',
+                          nargs=1,
+                          dest='model',
+                          help='Enter the model to describe')
+
+    cmd_parser.set_defaults(func=desc)
+
+
+def get_project_description(project_path):
+    
+    config_file = os.path.join(project_path, global_config.DLV_DIR, global_config.CONFIG_FILE)
+    config_dict = {}
+    with open(config_file, 'r') as f:
+        config_dict = json.load(f)
+
+    return config_dict['description']
+
+
+def get_history_of_files(project_path):
+
+    files_history = {}
+    current_branch = global_config.MASTER_BRANCH
+    project_branch_path = os.path.join(project_path, global_config.DLV_DIR, current_branch)
+
+    # open history file and get author message and date for each version
+    commit_log_file = os.path.join(project_branch_path, global_config.COMMIT_LOG_FILE) 
+    commit_history = {}
+
+    with open(commit_log_file, 'r') as f:
+        commit_history = json.load(f)
+
+    # open last commit file and get list of files and versions
+    last_commit_file = global_config.get_last_commit_version_file(project_branch_path)
+    
+    with open(last_commit_file, 'r') as f:
+        last_commit_dict = json.load(f)
+        for key in last_commit_dict.keys():
+            files_history[key] = {}
+            files_history[key]['version'] = last_commit_dict[key]
+            files_history[key]['author'] = commit_history["commit." + str(last_commit_dict[key])]['author']
+            files_history[key]['message'] = commit_history["commit." + str(last_commit_dict[key])]['message']
+            files_history[key]['date'] = commit_history["commit." + str(last_commit_dict[key])]['date']
+
+    # return the dictionary
+    return files_history
+
+def printTable(list_dict):
+
+    if list_dict == {}:
+        print("No model found in the user directory")
+
+    for k,v in list_dict.iteritems():
+
+        print(k + " => " + v['description'])
+        print "{:<25} {:<4} {:<15} {:<15} {:<20}".format('file', 'version', 'author', 'message', 'date')
+
+        for key,val in v['files'].iteritems():
+            print "{:<25} {:<4} {:<15} {:<15} {:<20}".format(key, val['version'], val['author'], val['message'], val['date'])
+    
+        print("\n\n")
+
+def desc(args = {}):
+
+    # Check whether the username is specified or not
+    if args.username == None:
+        print("FATAL: Please specify the username to list the projects")
+        sys.exit(0)
+
+    # Check whether the username is there is on server or not
+    user_server_dir = os.path.join(global_config.SERVER_PROJECT_DIR, args.username[0])
+    
+    if not os.path.isdir(user_server_dir):
+        print("Invalid Username")
+        sys.exit(0)
+
+    list_dict = {} # containes the list of projects and its lineages
+
+    # Get list of all projects in the user dir
+    for folder in os.listdir(user_server_dir):
+
+         if not folder == args.model[0]:
+             continue
+         list_dict[folder] = {}
+
+         # For each project get the description
+         project_path = os.path.join(user_server_dir, folder)
+         list_dict[folder]['description'] = get_project_description(project_path)
+         
+         # For each project get the history ( file, version, author, message, date )
+         list_dict[folder]['files'] = get_history_of_files(project_path)
+
+    printTable(list_dict)
---
+++ push.py
@@ -0,0 +1,102 @@
+import os
+import sys
+import json
+import global_config
+import shutil
+from distutils.dir_util import copy_tree
+from status import get_push_status
+
+def handle_options_push(cmd_parser):
+
+    cmd_parser.add_argument('-v', '--version',
+                          help="version",
+                          action="store_true")
+
+    cmd_parser.add_argument('-u','--username',
+                          nargs=1,
+                          dest='username',
+                          help='Enter the author address')
+
+    cmd_parser.add_argument('-d','--description',
+                          nargs=1,
+                          dest='description',
+                          help='Enter the description of project')
+
+    cmd_parser.set_defaults(func=push)
+
+def push(args = {}):
+
+    if not global_config.check_dlv_exists():
+        print("No dlv repository exists")
+        sys.exit()
+
+    dlv_dir = os.path.join(global_config.root_dir, global_config.DLV_DIR)
+
+    current_branch = global_config.get_current_branch()
+    branch_path = os.path.join(dlv_dir, current_branch)
+
+    if get_push_status(current_branch) == "Updated":
+       print("All files are up-to-date in the current branch: " + current_branch)
+       sys.exit(0) 
+
+    # Check for project name and the username
+    # If no project name get the basename 
+    # If no username request for the username
+    dlv_config_file = os.path.join(dlv_dir, global_config.CONFIG_FILE)
+
+    config_dict = {}
+    with open(dlv_config_file, 'r') as f: 
+        try: config_dict = json.load(f)
+        except: pass
+
+    if 'project_name' not in config_dict:
+        config_dict['project_name'] = os.path.basename(global_config.root_dir)
+
+    if 'username' not in config_dict and args.username == None:
+        print("FATAL: Please enter the username");
+        sys.exit(0)
+    elif 'username' not in config_dict:
+        config_dict['username'] = args.username[0]
+
+    if 'description' not in config_dict and args.description == None:
+        print("FATAL: Please enter the description of project");
+        sys.exit(0)
+    elif 'description' not in config_dict:
+        config_dict['description'] = args.description[0]
+
+    with open(dlv_config_file, 'w') as f:
+        f.write(json.dumps(config_dict, indent=4))
+
+    # Push the files to the specified user project directory locally
+    # Check if the server directory exists and if not create it
+    server_dir = global_config.SERVER_PROJECT_DIR
+    if not global_config.create_directory(server_dir):
+        print("Problem with the main server directory")
+        sys.exit(0)    
+
+    # Check if the user directory exists and if not create it
+    user_dir = os.path.join(server_dir, config_dict['username'])
+    if not global_config.create_directory(user_dir):
+        print("Problem with the user directory in the server")
+        sys.exit(0)
+
+    # Check if the users project exists and if not upload whole files ( check which branch ) 
+    project_dir = os.path.join(user_dir, config_dict['project_name'])
+    
+    if not global_config.create_directory(project_dir):
+        print("Problem with the users project directory in the server")
+        sys.exit(0)
+
+    for root, dirs, files in os.walk(dlv_dir):
+        
+        # If folder is DLV_DIR then create dir within server and push all files
+        if os.path.basename(root) == global_config.DLV_DIR:
+            project_dlv_dir = os.path.join(project_dir, global_config.DLV_DIR)
+            global_config.create_directory(project_dlv_dir)
+            for f in files:
+                shutil.copy(os.path.join(root, f), os.path.join(project_dlv_dir, f))
+
+        # If folder is branch then create dir and push all files within it
+        if os.path.basename(root) == current_branch:
+            project_branch_dir = os.path.join(project_dir, global_config.DLV_DIR, current_branch)
+            copy_tree(root, project_branch_dir)
